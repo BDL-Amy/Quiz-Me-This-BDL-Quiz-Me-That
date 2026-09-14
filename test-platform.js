3d0cfe10a7fd876ce80f82d025a3f27e1a88f70f
BDL-Amy <amaryllis.dem.nuytten@gmail.com>
2026-09-14 13:58:32 +0200

    Load BDL quiz questions 51-60

--- test-platform.js
+++ test-platform.js
@@ -1,3 +1,4 @@
 /* TEST PLATFORM LOADER */
+document.write('<script src="questions-51-60.js?v=20260914"><\/script>');
 document.write('<script src="test-platform-core.js?v=20260904"><\/script>');
 document.write('<script src="test-results.js?v=20260904"><\/script>');
